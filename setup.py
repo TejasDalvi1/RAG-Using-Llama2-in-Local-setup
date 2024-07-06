f53d179ed3d000d77a3ddc2f3f9d6eb6cbefa416
Tejas Dalvi <tejasdalvi.coep@gmail.com>
2024-07-06 19:12:54 +0530

    Loaded Llama2 model and ran inference.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'RAG Using Llama2',
+    version= '0.0.2',
+    author= 'Tejas Dalvi',
+    author_email= 'tejasdalvi.coep@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
